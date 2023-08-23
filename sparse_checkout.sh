242a21a2cd4edc6af6e287500045f7a369897e55
Jahed Naghipoor <42733403+JahedNaghipoor@users.noreply.github.com>
2023-08-23 14:28:43 +0200

    Create sparse_checkout.sh

---
+++ sparse_checkout.sh
@@ -0,0 +1,8 @@
+infrastructure_version=terraform   #options: terraform / bicep 
+project_type=classical      #options: classical / cv / nlp
+mlops_version=aml-cli-v2   #options: aml-cli-v2 / python-sdk-v1 / python-sdk-v2 / rai-aml-cli-v2
+orchestration=azure-devops #options: github-actions / azure-devops
+git_folder_location='/Users/jaheed.naghipoor/Projects/Azure-MLOps'  #replace with the local root folder location where you want to create the project folder
+project_name=AzureML-MLOps   #replace with your project name
+github_org_name=JahedNaghipoor   #replace with your github org name
+project_template_github_url=https://github.com/azure/mlops-project-template 
